cf8f04ffd2acf51f21024ce9b55812dc326be90a
wei-qiu <qiuwei55@hotmail.com>
2018-09-03 16:59:40 -0500

    add an ember data model

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -1,9 +1,11 @@
 import { module, test } from 'qunit';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage'
 import { click, visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 
 module('Acceptance | list rentals', function(hooks) {
   setupApplicationTest(hooks);
+  setupMirage(hooks);
 
   test('should show rentals as the home page', async function (assert) {
   });
